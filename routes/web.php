d6703e1e7b081034feb4ca54ede099eecdaf28da
Noro190695 <norojan56@mail.ru>
2021-07-18 17:44:42 +0400

    dploying

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Room;
 use App\Http\Controllers\Gamer;
 use App\Http\Controllers\Roole;
 use App\Http\Controllers\Index;
-
+Route::get('/', [Index::class, 'index']);
 Route::prefix('api')->group(function () {
     Route::prefix('room')->group(function () {
         Route::get('/', [Room::class, 'index']);
@@ -21,4 +21,4 @@ Route::prefix('api')->group(function () {
     });
 });
 
-Route::get('/', [Index::class, 'index']);
+
