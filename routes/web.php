df07e65724e1452c1187a08ffa89f29b490b776c
Noro190695 <norojan56@mail.ru>
2021-07-18 17:51:05 +0400

    dploying

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,20 @@ use App\Http\Controllers\Gamer;
 use App\Http\Controllers\Roole;
 use App\Http\Controllers\Index;
 Route::get('/', [Index::class, 'index']);
-//Route::prefix('api')->group(function () {
-//
-//    Route::prefix('room')->group(function () {
-//        Route::get('/', [Room::class, 'index']);
-//        Route::post('/add', [Room::class, 'create']);
-//    });
-//    Route::prefix('roole')->group(function () {
-//        Route::get('/', [Roole::class, 'index']);
-//        Route::post('/add', [Roole::class, 'create']);
-//    });
-//    Route::prefix('gamer')->group(function () {
-//        Route::get('/', [Gamer::class, 'index']);
-//        Route::get('/add', [Gamer::class, 'create']);
-//    });
-//});
+Route::prefix('api')->group(function () {
+
+    Route::prefix('room')->group(function () {
+        Route::get('/', [Room::class, 'index']);
+        Route::post('/add', [Room::class, 'create']);
+    });
+    Route::prefix('roole')->group(function () {
+        Route::get('/', [Roole::class, 'index']);
+        Route::post('/add', [Roole::class, 'create']);
+    });
+    Route::prefix('gamer')->group(function () {
+        Route::get('/', [Gamer::class, 'index']);
+        Route::get('/add', [Gamer::class, 'create']);
+    });
+});
 
 
